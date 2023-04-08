d65c43a10c8f1dd4d58cd07528cfd940a1b71bf1
HomePC <HomePC@DESKTOP-DAFVJ5I>
2023-04-09 02:11:57 +0300

    Добавьте файлы проекта.

---
+++ RDA.TaskSQL1/View/InfoWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using RDA.TaskSQL1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RDA.TaskSQL1.View
+{
+    /// <summary>
+    /// Логика взаимодействия для InfoWindow.xaml
+    /// </summary>
+    public partial class InfoWindow : Window
+    {
+        private UserInfoEntities _db = new UserInfoEntities();
+        public InfoWindow()
+        {
+            InitializeComponent();
+
+            DGInfo.ItemsSource = _db.Users.ToList();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            Close();
+        }
+    }
+}
---
+++ RDA.TaskSQL1/View/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using RDA.TaskSQL1.Model;
+using RDA.TaskSQL1.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RDA.TaskSQL1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private UserInfoEntities _db = new UserInfoEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnReg_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                if (string.IsNullOrEmpty(TbLogin.Text) ||
+                    string.IsNullOrEmpty(PbPassword.Password) ||
+                    string.IsNullOrEmpty(TbEmail.Text))
+
+
+                {
+                    MessageBox.Show($"Не все строки заполнены!");
+                }
+                else
+                {
+                    _db.Users.Add(new Users()
+                    {
+                        login = TbLogin.Text,
+                        password = PbPassword.Password,
+                        email = TbEmail.Text
+                    }); ;
+                    _db.SaveChanges();
+                    MessageBox.Show($"Успех!");
+                    TbLogin.Text = string.Empty;
+                    PbPassword.Password = string.Empty;
+                    TbEmail.Text = string.Empty;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Error 3");
+            }
+        }
+
+        private void BtnRun_Click(object sender, RoutedEventArgs e)
+        {
+            new InfoWindow().Show();
+            Close();
+        }
+    }
+}
